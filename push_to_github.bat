a42b1524b566e7e12978eafb0a12894302d08612
virajkadu2006-creator <virajkadu2006@gmail.com>
2026-09-09 00:20:45 +0530

    Add push helper script

---
+++ push_to_github.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Pushing MPLADS Risk Intelligence System to GitHub...
+git remote set-url origin https://github.com/virajkadu2006-creator/mplads-risk-intelligence.git
+git branch -M main
+git push -u origin main
+echo Done!
+pause
